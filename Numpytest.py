15cb358f1791c4b78676ae734f953fa61062bcef
Malik Polat <malik@Maliks-MacBook-Air.local>
2025-10-02 14:14:49 +0200

    Added tan(x)

--- Numpytest.py
+++ Numpytest.py
@@ -5,10 +5,12 @@ import matplotlib.pyplot as plt
 x = np.linspace(0, 10, 100)   # 100 points between 0 and 10
 y1 = np.sin(x)                 # sine function
 y2 = np.cos(x)                 # cosine function
+y3 = np.tan(x)
 
 # Plot
 plt.plot(x, y1, label="sin(x)")
 plt.plot(x, y2, label="cos(x)")
+plt.plot(x, y3, label="tan(x)")
 plt.title("Multiple Functions")
 plt.xlabel("x")
 plt.ylabel("y")
