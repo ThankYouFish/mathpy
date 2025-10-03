76117dbafdd719d56fb40b3ebcd28cacf8b08bd2
Malim <malik@Mac.fritz.box>
2025-10-03 04:24:16 +0200

    Added documentation

--- FAS_Sensor.py
+++ FAS_Sensor.py
@@ -3,18 +3,22 @@ import matplotlib.pyplot as plt
 import math
 from scipy import integrate
 
-t = np.arange(0., 0.5, 0.01) #t-Array für meine Werte auf x-Achse
+# Declarations
+t = np.arange(0., 0.5, 0.01) # t-Array for values on x-Axis
+y_0 = 15 # Peak ramp value
+t_up = 0.5 # Period length
+m = 15/0.4 # Gradient
 
-y_0 = 15
-t_up = 0.5
-m = 15/0.4
-y = np.where(t <= 0.4, m * t, 0)
-y_eff = math.sqrt(1/t_up * integrate.trapezoid(y**2, t, 0.01))
+# Functions
+y = np.where(t <= 0.4, m * t, 0) # Create ramp signal from 0 to 0.4
+y_eff = math.sqrt(1/t_up * integrate.trapezoid(y**2, t, 0.01)) # y_eff to calculate RMS
 
+# Plotting
 plt.plot(t, y, label="Periodische Beschleunigung")
 plt.grid(True)
 plt.xlabel("t")
 plt.ylabel("a")
 plt.show()
 
+# Print result
 print(f"RMS: {y_eff}")
